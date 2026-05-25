585d5d542b550eb3110fe66e300de68707fda966
antonio gomes <antoniomigom@gmail.com>
2026-05-25 18:00:01 -0300

    first commit: base project

---
+++ src/main/java/com/atwo/currencies_api/CurrenciesApiApplication.java
@@ -0,0 +1,13 @@
+package com.atwo.currencies_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrenciesApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/atwo/currencies_api/currency/controllers/CurrencyController.java
@@ -0,0 +1,5 @@
+package com.atwo.currencies_api.currency.controllers;
+
+public class CurrencyController {
+
+}
