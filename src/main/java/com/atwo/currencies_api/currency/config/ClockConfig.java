6e247d12dbb2684eb1d3026645f05e112fc05555
antonio gomes <antoniomigom@gmail.com>
2026-07-10 15:42:22 -0300

    add injectable clock to project

---
+++ src/main/java/com/atwo/currencies_api/currency/config/ClockConfig.java
@@ -0,0 +1,15 @@
+package com.atwo.currencies_api.currency.config;
+
+import java.time.Clock;
+import java.time.ZoneId;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ClockConfig {
+
+    @Bean
+    public Clock clock() {
+        return Clock.system(ZoneId.of("America/Sao_Paulo"));
+    }
+}
