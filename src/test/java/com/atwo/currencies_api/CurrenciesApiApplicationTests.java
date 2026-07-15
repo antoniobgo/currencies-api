2916c4fd7dd84854917f77089747f8ab41d9a6a0
antonio gomes <antoniomigom@gmail.com>
2026-07-14 21:29:45 -0300

    config: CI

--- src/test/java/com/atwo/currencies_api/CurrenciesApiApplicationTests.java
+++ src/test/java/com/atwo/currencies_api/CurrenciesApiApplicationTests.java
@@ -2,9 +2,25 @@ package com.atwo.currencies_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@Testcontainers
 @SpringBootTest
 class CurrenciesApiApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+	}
 
 	@Test
 	void contextLoads() {
